2010a5cd39217d2396312ca79100f53818793cd2
Tarun Gupta <97735684+TarunxGupta@users.noreply.github.com>
2024-11-30 17:35:32 +0530

    commit

--- train_classifier.py
+++ train_classifier.py
@@ -1,28 +1,29 @@
 import pickle
-
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+import tensorflow as tf
 import numpy as np
 
-
+# Load preprocessed data
 data_dict = pickle.load(open('./data.pickle', 'rb'))
+data = np.array(data_dict['data'])
+labels = np.array(data_dict['labels'], dtype=int)
 
-data = np.asarray(data_dict['data'])
-labels = np.asarray(data_dict['labels'])
+# Train-test split
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, stratify=labels)
 
-x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+# Build a neural network model
+model = tf.keras.Sequential([
+    tf.keras.layers.InputLayer(input_shape=(42,)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(len(set(labels)), activation='softmax')
+])
 
-model = RandomForestClassifier()
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train)
+# Train the model
+model.fit(x_train, y_train, epochs=20, validation_data=(x_test, y_test))
 
-y_predict = model.predict(x_test)
-
-score = accuracy_score(y_predict, y_test)
-
-print('{}% of samples were classified correctly !'.format(score * 100))
-
-f = open('model.p', 'wb')
-pickle.dump({'model': model}, f)
-f.close()
+# Save the model
+model.save('gesture_model.h5')
